afdd0c5d9f94581b67a4acb1afb19a91f63949dd
mayurpaliwal55 <mayur.paliwal@fiftyfivetech.io>
2022-10-18 17:54:21 +0530

    done

--- service.py
+++ service.py
@@ -12,12 +12,11 @@ def is_valid_input(x,status_code=200):
             try:
                 if isinstance(int(x),int):
                     item =  f"data type 'int' not allowed"
-                    return JSONResponse(status_code=429,content=item)
+                    return JSONResponse(status_code=400,content=item)
             except:
                 item =  f"data type 'float' not allowed"
-                return JSONResponse(status_code=429,content=item)
+                return JSONResponse(status_code=400,content=item)
     except:
         if x=="True" or x=="False":
             item =  f"Boolean Data Type Not Allowed"
             return JSONResponse(status_code=503,content=item)
-        
